d8aaffe257030f837293d924bd1e56f52fe33a03
ranqing <ranqing@didichuxing.com>
2016-12-22 22:49:03 +0800

    add mono calibration when no mono results prepared

---
+++ source/common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include "../../../Qing/qing_common.h"
+
+enum MODE { WITH_MONO_RESULT, WITHOUT_MONO_RESULT };
+enum Pattern { CHESSBOARD, ASYMMETRIC_CIRCLES_GRID} ;
+
+#endif // COMMON_H
